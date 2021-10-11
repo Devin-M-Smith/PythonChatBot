5b17566d351c789f721e1822e403639675462d9b
Devin Smith <dev6785@gmail.com>
2021-10-11 18:51:43 -0400

    0.0.1

--- PythonChatBot/PythonChatBot.py
+++ PythonChatBot/PythonChatBot.py
@@ -1,6 +1,9 @@
 def central():
-    x = input("Hello \n")
-    print(x)
+    
+    x = input("Hello \nYou: ")
+    while x.lower() != "shutdown":
+        print("You said: " + x)
+        x = input("You: ")
 
 
 print("Welcome to PyChat! \nPlease see Readme.md for important updates and features.")

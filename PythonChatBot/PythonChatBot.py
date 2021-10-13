39d1789ac759d0eeafd6b12d9aedd4362ecd8430
Devin Smith <dev6785@gmail.com>
2021-10-13 15:16:09 -0400

    Comments and Cleanup

--- PythonChatBot/PythonChatBot.py
+++ PythonChatBot/PythonChatBot.py
@@ -1,15 +1,31 @@
 from wordHandler import fixIt
 from time import sleep
-def central():
+import sys
+def prnt(x):              #Alternative Print method for single line printing
+    sys.stdout.write(x)
+    sys.stdout.flush()
+
+def central():              #Main Function
     x = ""
-    while x != "shutdown":
-        x = input("You: ")
-        x = fixIt(x)
+    while x != "shutdown":          #Loop for user interaction
+        x = input("You: ")          #Get text input
+        x = fixIt(x)                #Scrub input of punctuation and make lowercase
         print("You said: " + x)
 
-    print("Shutting down")
+    
         
 
 
-print("Welcome to PyChat! \nPlease see Readme.md for important updates and features. \nHello!")
-central()
+print("Welcome to PyChat! \nPlease see Readme.md for important updates and features. \nHello!") #Welcome Message
+
+central() #Call Main Function
+
+
+prnt("Shutting Down")   #Shutdown Message
+sleep(.5)
+prnt(".")
+sleep(.5)
+prnt(".")
+sleep(.5)
+prnt(".")
+sleep(.5)
